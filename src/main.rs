b9e2510a935c29d7591fdcb69213ffa0bf08d4c6
qianxi0410 <894871277@qq.com>
2021-08-31 23:26:00 +0800

    Add fn to create basic ppm file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+use std::fmt::write;
+use std::fs::{self, OpenOptions};
+
+mod color;
 mod vec3;
 
-fn main() {
-    let mut v = vec3::Vec3::new(1, 2, 3);
-    v += 2;
+use std::fs::File;
+use std::io::{self, Write};
 
-    println!("{}", v.x);
+use crate::color::write_color;
+
+fn main() -> io::Result<()> {
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    let mut fp = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("./result/scene1.ppm")
+        .expect("cannot open file");
+
+    fp.write(format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT).as_bytes());
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let pixel_color = vec3::Color::new(
+                i as f64 / (IMAGE_WIDTH - 1) as f64,
+                j as f64 / (IMAGE_HEIGHT - 1) as f64,
+                0.25,
+            );
+
+            write_color(&mut fp, pixel_color);
+        }
+    }
+    Ok(())
 }
