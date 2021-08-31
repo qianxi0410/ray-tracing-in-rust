5205f967063e95c95d4b0bb830e25b16dd15e3ae
qianxi0410 <894871277@qq.com>
2021-08-31 23:25:32 +0800

    Change write_color fn

--- src/color.rs
+++ src/color.rs
@@ -2,10 +2,15 @@ use crate::vec3::Color;
 use std::io;
 
 pub fn write_color(fp: &mut impl io::Write, color: Color) -> io::Result<()> {
-    fp.write(&[
-        (255.999 * color.x) as u8,
-        (255.999 * color.y) as u8,
-        (255.999 * color.z) as u8,
-    ])?;
+    fp.write(
+        format!(
+            "{} {} {}\n",
+            (255.999 * color.x) as u8,
+            (255.999 * color.y) as u8,
+            (255.999 * color.z) as u8,
+        )
+        .as_bytes(),
+    )?;
+
     Ok(())
 }
