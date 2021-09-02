cf4ad44eee8cf05190db63f03f285a0437c40e8f
qianxi0410 <894871277@qq.com>
2021-09-02 15:34:43 +0800

    Simplfy the hit_sphere code

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,32 @@ fn hit_sphere(center: &Point3d, radius: f64, r: &Ray) -> bool {
     discriminant > 0.0
 }
 
+fn hit_sphere_color(center: &Point3d, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - *center;
+    let a = r.direction().length_squared();
+    let half_b = oc.dot(&r.direction());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) / a
+    }
+}
+
+fn ray_color_sphere_color(r: &Ray) -> Color3d {
+    let mut t = hit_sphere_color(&Point3d::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let N = (r.at(t) - Vec3d::new(0.0, 0.0, -1.0)).unit_vector();
+        return 0.5 * Color3d::new(N.x + 1.0, N.y + 1.0, N.z + 1.0);
+    }
+    let unit_direction = r.direction().unit_vector();
+    t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * Color3d::only(1.0) + t * Color3d::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    scene3();
+    scene4();
 }
 
 fn scene1() -> io::Result<()> {
@@ -137,6 +161,45 @@ fn scene3() -> io::Result<()> {
                 lower_left_corner + u * horizontal + v * vertical - origin,
             );
             let pixel_color = ray_color_sphere(&r);
+            write_color(&mut fp, pixel_color);
+        }
+    }
+    Ok(())
+}
+
+fn scene4() -> io::Result<()> {
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    let origin = Point3d::zero();
+    let horizontal = Vec3d::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical = Vec3d::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3d::new(0.0, 0.0, FOCAL_LENGTH);
+
+    let mut fp = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("./result/circle_color.ppm")
+        .expect("cannot open file");
+
+    fp.write(format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT).as_bytes());
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_color = ray_color_sphere_color(&r);
             write_color(&mut fp, pixel_color);
         }
     }
