f581bf67b6bccb38b416519d1537f3aee215b90d
qianxi0410 <894871277@qq.com>
2021-09-06 11:20:38 +0800

    Update utils

---
+++ src/utils.rs
@@ -0,0 +1,23 @@
+use rand::{self, Rng};
+use std::f64::consts::PI;
+
+#[inline]
+pub fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * PI / 180.0
+}
+
+pub fn random_double() -> f64 {
+    // [0, 1)
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0.0..1.0)
+}
+
+pub fn random_range(min: f64, max: f64) -> f64 {
+    // [min, max)
+    if min == max {
+        min
+    } else {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(min..max)
+    }
+}
